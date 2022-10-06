6b3ac508f745706f62bbb34af6dd61d8d502fa4b
juanvoo <juanmoreiracosta2016@gmail.com>
2022-10-06 15:06:16 +0000

    cambios para realizar todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [tarea, setTarea] = useState([]);
+  const [listaDeTareas, setListadeTareas] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    // add the todo to the list
+    setTarea([...tarea, newTodo]);
+
+    // clear input box
+    setListadeTareas("");
+  };
+
+  const deleteTodo = (id) => {
+    // Filter out todo with the id
+    const newList = tarea.filter((todo) => todo.id !== id);
+
+    setTarea(newList);
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+  <div className="card-body">
+      <h1 className="card-title d-flex justify-content-center display-1 text-black-50">todos</h1>
+      <input
+        type="text"
+        className="form-control"
+        id="exampleInputTarea"
+        aria-describedby="emailHelp"
+        onChange={(e) => setListadeTareas(e.target.value)}
+        value={listaDeTareas}
+      />
+      <button className="mt-3 bg-info w-100" onClick={() => addTodo(listaDeTareas)}>Agregar a la lista</button>
+      <div className="card mt-3">
+      <ul className="list-group-flush">
+        {tarea.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button className="bg-transparent border-0 btn-lg float-right" onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+  );
+}
 
 export default Home;
